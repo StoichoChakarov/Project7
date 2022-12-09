46a72397593245c96dae43b235eecd9a1dbb37a9
student <student@DESKTOP-LFKURTD>
2022-12-09 08:59:09 +0200

    Add project files.

---
+++ Project7/Models/TaskModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project7.Models
+{
+    public class TaskModel
+    {
+        public int TaskId { get; set; }
+
+        public int UserId { get; set; }
+        public string TaskTitle { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DeadLine { get;set;}
+        public string TaskDescr { get; set; }
+        public int TaskListId { get; set; }
+    }
+}
---
+++ Project7/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Project7.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhnoneNumber { get; set; }
+
+        public List<TaskListModel> TaskList { get; set; }
+    }
+}
